7636be77bc5b907e79516c1d375950c470043277
ambv <ambv@users.noreply.github.com>
2020-10-16 02:09:15 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -10,7 +10,7 @@ class EdgedbCliNightly < Formula
   end
 
   def self.download_file_path
-    "edgedb-cli_1.0.0-alpha.6+d20201015.gf94014b25_2020101500~nightly"
+    "edgedb-cli_1.0.0-alpha.6+d20201016.g956da0719_2020101600~nightly"
   end
 
   def self.download_sha256_checksum
@@ -21,7 +21,7 @@ class EdgedbCliNightly < Formula
   homepage "https://github.com/edgedb/edgedb-cli"
   url "https://packages.edgedb.com/archive/#{plat}.nightly/#{download_file_path}?edgedb.nightly",
     :using => :nounzip
-  version "1.0.0-alpha.6+d20201015.gf94014b25"
+  version "1.0.0-alpha.6+d20201016.g956da0719"
   sha256 download_sha256_checksum
   bottle :unneeded
 
