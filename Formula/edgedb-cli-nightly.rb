9003da78408250b0e5b4729ba143d2dc2e41ace4
elprans <elprans@users.noreply.github.com>
2022-08-30 02:55:37 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -5,13 +5,13 @@ class EdgedbCliNightly < Formula
 
   on_macos do
     if Hardware::CPU.physical_cpu_arm64?
-      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.840+5d39fe0?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.840+0959ed2?edgedb.nightly",
         using: :nounzip
-      sha256 "e441d6f342d180bcdb7cfd942e6ca2507366e89e4f5f595c84c74f1c6a455854"
+      sha256 "86a1a4a4f1bc11492b98cd9df5336c1a88fe565e1fa3c51a07c7f63b17585fb8"
     elsif Hardware::CPU.intel?
-      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.840+5e65a60?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.840+1ef3109?edgedb.nightly",
         using: :nounzip
-      sha256 "c245d70e77ebeaa35d74c661b3fbd759aae7c1eb51cb0694a734da408399506d"
+      sha256 "2b2a745c09ca9fb3426f5a297c5f4454703d91f963ddb19e5bde1d8ada618752"
     else
       odie "Unsupported CPU architecture!"
     end
@@ -19,9 +19,9 @@ class EdgedbCliNightly < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-2.1.0-dev.840+92fe7cd?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-2.1.0-dev.840+3ac5fbf?edgedb.nightly",
         using: :nounzip
-      sha256 "01f4bafcf955613e96447cba1935c01d46b73fa3e24b08c297187237fe25f695"
+      sha256 "2ab36a06a864d12bed34f8863e8a075749d2426dd00ee0c061b78a09b89ede28"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-2.1.0-dev.840+3bdc1f5?edgedb.nightly",
         using: :nounzip
