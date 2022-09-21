6a963202b68e70f6c5f1bcffb1cd45d32e689629
elprans <elprans@users.noreply.github.com>
2022-09-21 02:57:50 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -5,9 +5,9 @@ class EdgedbCliNightly < Formula
 
   on_macos do
     if Hardware::CPU.physical_cpu_arm64?
-      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.846+f3d0737?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.846+825397c?edgedb.nightly",
         using: :nounzip
-      sha256 "98d65d6e013b703ae62c3aeccdc15a3d4955feee05cd2523d724d5a1214513de"
+      sha256 "4ab6b798dd6aee0fc00298b5e6e7c19a5226023cefde6638afe7d10f528b7583"
     elsif Hardware::CPU.intel?
       url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-2.1.0-dev.846+2e64d97?edgedb.nightly",
         using: :nounzip
