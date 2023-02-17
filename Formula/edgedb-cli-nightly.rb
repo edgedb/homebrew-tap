0acf4318a4700e51a9b9c34e2e2ac9a6b70ea6e4
elprans <elprans@users.noreply.github.com>
2023-02-17 02:28:19 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -9,9 +9,9 @@ class EdgedbCliNightly < Formula
         using: :nounzip
       sha256 "86c3a67b8d8b1ce50b00e5b1f6e5dabe08b02e2187684f005774bf618517cae1"
     elsif Hardware::CPU.intel?
-      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-2.3.0-dev.912+c39dc49?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-2.3.0-dev.912+583193a?edgedb.nightly",
         using: :nounzip
-      sha256 "a9f9588579e4661ab283cb1c68ab07e6236fd1a83edfa466da23a68568cbcffd"
+      sha256 "60d6e12affd7825643890bb31795b28998e98a9abb7da7dc843494dd8b66ea05"
     else
       odie "Unsupported CPU architecture!"
     end
@@ -19,13 +19,13 @@ class EdgedbCliNightly < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-2.3.0-dev.912+b2d8d14?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-2.3.0-dev.912+356d25b?edgedb.nightly",
         using: :nounzip
-      sha256 "a7e9049a7e58d8d1631fac0909295a797ffd717c67e3a5427544cb9ec3f5d9db"
+      sha256 "902228d7dc42bb750910f53021e0210a33ed864743b3fffd24307d00298fafcb"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-2.3.0-dev.912+678682c?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-2.3.0-dev.912+596e350?edgedb.nightly",
         using: :nounzip
-      sha256 "21eb8536a3805c6e56803712a30ac4a4e07c6ca0e5380baf3fb01893a8df04c3"
+      sha256 "34053bfdb2ba957c61491ea70a636bafc2c81a49ea6f4f6bf69213af439e9db4"
     else
       odie "Unsupported CPU architecture!"
     end
