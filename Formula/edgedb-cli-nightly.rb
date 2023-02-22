c5790b012c2b30ca4ccb957a715e9af38bfaf3d7
elprans <elprans@users.noreply.github.com>
2023-02-22 02:26:35 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -5,13 +5,13 @@ class EdgedbCliNightly < Formula
 
   on_macos do
     if Hardware::CPU.physical_cpu_arm64?
-      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.913+3ed516f?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.913+355c0d9?edgedb.nightly",
         using: :nounzip
-      sha256 "4b43678380bb89b9df02959e460ef7f0521c7be6c64b5ba654f9b2ef7a03027a"
+      sha256 "9c43e0a79644d50979f451421e3f185ed58b8a0be9b4397ba6e1788f2e95102f"
     elsif Hardware::CPU.intel?
-      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.913+494242f?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.913+2ee5851?edgedb.nightly",
         using: :nounzip
-      sha256 "951d4f254cde9a2b593f388e3db3ffa2c7996ff077e584e9b8a98cd3b26802c2"
+      sha256 "42b1392214644f10515c21ce863bf532c7e712a45b9640d3b2027737df906209"
     else
       odie "Unsupported CPU architecture!"
     end
@@ -23,9 +23,9 @@ class EdgedbCliNightly < Formula
         using: :nounzip
       sha256 "47f56385954f3bd974bb2b14ccc8f50b2f6a3133068e4c3301def729a4092ebb"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.913+7ae9445?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.913+1d3b06e?edgedb.nightly",
         using: :nounzip
-      sha256 "536aff4bf5d19878c4c6710168952a6484b7164442ddec25be69a4af5998dd75"
+      sha256 "125a32104292097f5eb4339e8140c9c47e5733d0fa59421fd225ebef2ccc87c6"
     else
       odie "Unsupported CPU architecture!"
     end
--- Formula/edgedb-cli.rb
+++ Formula/edgedb-cli.rb
@@ -1,17 +1,17 @@
 class EdgedbCli < Formula
   desc "Latest release of the EdgeDB command-line client (edgedb)"
   homepage "https://github.com/edgedb/edgedb-cli"
-  version "2.3.0"
+  version "2.3.1"
 
   on_macos do
     if Hardware::CPU.physical_cpu_arm64?
-      url "https://packages.edgedb.com/archive/aarch64-apple-darwin/edgedb-cli-2.3.0+c4d9e41?edgedb.release",
+      url "https://packages.edgedb.com/archive/aarch64-apple-darwin/edgedb-cli-2.3.1+5d93f42?edgedb.release",
         using: :nounzip
-      sha256 "d768014feffa4a7d3ea89446ac8744207bdc713e32cb6042604cd7e087c74e93"
+      sha256 "7a26c34b2bf444732ea705bb54c0de3deca7524d6063b40f57365ee4159cc931"
     elsif Hardware::CPU.intel?
-      url "https://packages.edgedb.com/archive/x86_64-apple-darwin/edgedb-cli-2.3.0+c4d9e41?edgedb.release",
+      url "https://packages.edgedb.com/archive/x86_64-apple-darwin/edgedb-cli-2.3.1+5d93f42?edgedb.release",
         using: :nounzip
-      sha256 "8dc1baa64ab5aba5a5a2cf787a671b524c2bc048ec04ac5fe4c4aea98d94fffc"
+      sha256 "10f2a1474a96e3bd3d3cc65c6a25c92be1d215980ecb16251765cbfad043482f"
     else
       odie "Unsupported CPU architecture!"
     end
@@ -19,13 +19,13 @@ class EdgedbCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl/edgedb-cli-2.3.0+a8d47a2?edgedb.release",
+      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl/edgedb-cli-2.3.1+b4be149?edgedb.release",
         using: :nounzip
-      sha256 "9cf5b3dcc7f3a5a32e6f6c7f9884707aa3a136ecce82a61fe30b2cd5319f421b"
+      sha256 "af4de37ef9b9031a73fbaee4fed2a42ff036ba39fdcfb7e22c741625fcfb19a7"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl/edgedb-cli-2.3.0+51640c8?edgedb.release",
+      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl/edgedb-cli-2.3.1+ef99779?edgedb.release",
         using: :nounzip
-      sha256 "00c17adf51809ef5b90541c82df819b860521a608666a2df7b4234c3ff5bcbb0"
+      sha256 "3e6c0e7e535e18e12164589bf0dd0fe3e4baecc635adf1afd5aec23d0c2bccdc"
     else
       odie "Unsupported CPU architecture!"
     end
