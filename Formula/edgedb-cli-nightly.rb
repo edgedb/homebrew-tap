21b79e8d65bf50b308cedc734e861afcdaa1a219
elprans <elprans@users.noreply.github.com>
2023-04-16 02:18:08 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -23,9 +23,9 @@ class EdgedbCliNightly < Formula
         using: :nounzip
       sha256 "339c900ce37b1e886d3aed5dda845ca28afe450505abad9b0ef3694cfec2b4d9"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.947+09cfbcb?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.947+0988957?edgedb.nightly",
         using: :nounzip
-      sha256 "a309ce41bc954fcef2951f0bd20c54d7624a2ea1e97bc5682e5ea591b98a1490"
+      sha256 "6a0a4704b1810f69cceee59bdee541f6c816f7ba3c6ac38738ecf47f4a725990"
     else
       odie "Unsupported CPU architecture!"
     end
