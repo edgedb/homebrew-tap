cb57f53c2a4999da9dee36687f8ef9408a783cd7
elprans <elprans@users.noreply.github.com>
2023-04-24 02:16:23 +0000

    Auto-update package versions

--- Formula/edgedb-cli-nightly.rb
+++ Formula/edgedb-cli-nightly.rb
@@ -5,9 +5,9 @@ class EdgedbCliNightly < Formula
 
   on_macos do
     if Hardware::CPU.physical_cpu_arm64?
-      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.960+56f0250?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.960+40d95df?edgedb.nightly",
         using: :nounzip
-      sha256 "74f47237fbfbc82df5756bfa21d245e2f6056469084b734b64a4ce3958928b3a"
+      sha256 "9c262e35af909906b8d724e3329585e283ba4a5ca680ad6ddf328dc5dfac9a56"
     elsif Hardware::CPU.intel?
       url "https://packages.edgedb.com/archive/x86_64-apple-darwin.nightly/edgedb-cli-3.0.0-dev.960+213ae8a?edgedb.nightly",
         using: :nounzip
@@ -19,13 +19,13 @@ class EdgedbCliNightly < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.960+3caf63e?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/aarch64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.960+255a1c9?edgedb.nightly",
         using: :nounzip
-      sha256 "0b7bee880ca362042657fc45f8cf1f3e3425007b9693f69b53ef57e74de94004"
+      sha256 "c860c4b270e7c1f4846026a2d03566187bb648274b8526d3ce80e53a732475c7"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.960+dee5d68?edgedb.nightly",
+      url "https://packages.edgedb.com/archive/x86_64-unknown-linux-musl.nightly/edgedb-cli-3.0.0-dev.960+8f8acb0?edgedb.nightly",
         using: :nounzip
-      sha256 "0a53b52b3587dae44ef24d99e1b1b61f6e201d194beeecabb7055c0eaf9be104"
+      sha256 "83437d14a9b1d728503805dea01f699594034046f41b3bbefb51086e2ed0313d"
     else
       odie "Unsupported CPU architecture!"
     end
